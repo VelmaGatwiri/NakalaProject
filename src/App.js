1fde61a44b87d563ecfe95144b652936f67f6b4a
VelmaGatwiri <velma.gatwiri@strathmore.edu>
2023-03-01 11:43:29 +0300

    First commit of finalised backend development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [userbiodata, setbiodata] = useState([])
+
+useEffect(() => {
+
+
+  fetch(' http://127.0.0.1:8000/userBackend/biodata/', {
+    'method': 'GET',
+    headers: {
+      'Content-Type':'application/json',
+      'Authorization':'Token 6f2f0bfed775c7b292096a108b1acd25bcc3cb13'
+    }
+    
+  })
+  .then(resp => resp.json())
+  .then(resp => setbiodata(resp))
+  .catch(error => console.log(error))
+
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Nakala Project Exercise</h3>
+
+        {userbiodata.map(biodata => {
+          return <h2>{biodata}</h2>
+        })}
     </div>
+
   );
 }
 
---
+++ src/components/frontendapp.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Hello(){
+    return(
+        <h1>Inside Function Component</h1>
+    )
+}
+export default frontendapp
